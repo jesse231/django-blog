ff09f9d0feb53b28064f757d1503088da7a4532c
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2023-03-27 20:36:33 -0400

    Create list_posts and post_detail views

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+def list_posts(request):
+    posts = Post.objects.order_by('-pub_date')
+    return render(request, 'list_posts.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_details.html', {'post': post})
